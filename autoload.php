6450e4d5234caa84841ec4197642ba77f6910a0e
CarsaiMz <carsaimozambique@gmail.com>
2025-12-09 13:54:12 +0200

    Atualizar o autoload.php

--- autoload.php
+++ autoload.php
@@ -1,21 +1,24 @@
 <?php
-
 /* Loading composer packages */
 if(file_exists(__DIR__.'/vendor/autoload.php')) {
     require_once __DIR__.'/vendor/autoload.php';
 }
 
 spl_autoload_register(function ($class_name) {
-    // Remove o namespace 'carsaimz\' do início
-    $class_name = str_replace('carsaimz\\', '', $class_name);
-    
-    // Converte namespace para estrutura de diretórios
-    $class_path = implode('/', explode('\\', $class_name));
-    
-    // Caminho para o arquivo da classe
-    $file_path = __DIR__.'/'.$class_path.'.php';
-    
-    if(file_exists($file_path)) {
-        require_once $file_path;
+    // Para o namespace carsaimz
+    if (strpos($class_name, 'carsaimz\\') === 0) {
+        // Remove o namespace 'carsaimz\'
+        $relative_class = substr($class_name, strlen('carsaimz\\'));
+        
+        // Converte namespace para caminho de arquivo
+        $file = __DIR__ . '/src/' . str_replace('\\', '/', $relative_class) . '.php';
+        
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            // Log para debug
+            error_log("Autoload: Arquivo não encontrado: $file para classe: $class_name");
+        }
     }
 });
+?>
